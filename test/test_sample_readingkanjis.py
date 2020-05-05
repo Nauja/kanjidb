2771629062149f0f83a9592516a683dd33a120f8
Nauja <a@a.com>
2020-05-05 11:37:59 +0200

    Sample reading kanjis

--- test/test_sample_readingkanjis.py
+++ test/test_sample_readingkanjis.py
@@ -3,7 +3,6 @@ __all__ = ["SampleReadingKanjisTestCase"]
 import os
 import unittest
 from kanjidb import builder
-from kanjidb.builder.configuration import Configuration
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 SAMPLE_TXT = os.path.join(DATA_DIR, "sample_readingkanjis.yml")
@@ -11,14 +10,7 @@ SAMPLE_TXT = os.path.join(DATA_DIR, "sample_readingkanjis.yml")
 
 class SampleReadingKanjisTestCase(unittest.TestCase):
     def test(self):
-        config = Configuration()
-        config.load(SAMPLE_TXT)
-
-        result = builder.build(config)
-
-        self.assertTrue("kanjis" in result, "Invalid output")
-        kanjis = result["kanjis"]
-        self.assertEqual(kanjis, ["一", "二"], "Invalid result")
+        builder.main([SAMPLE_TXT])
 
 
 if __name__ == "__main__":
