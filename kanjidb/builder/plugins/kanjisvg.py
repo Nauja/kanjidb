78553ca3f415dbd250ee77715c1b9d90cf139e5b
Nauja <a@a.com>
2020-05-10 14:18:25 +0200

    don't always copy svg files

--- kanjidb/builder/plugins/kanjisvg.py
+++ kanjidb/builder/plugins/kanjisvg.py
@@ -9,7 +9,8 @@ class Plugin(PluginBase):
     @property
     def template_config(self):
         return {
-            "svg_dir": "svg/",
+            "svg_input_dir": "svg/",
+            "base_url": "http://127.0.0.1:8080/svg/",
             "inputs": [{"type": "var", "name": "db"}],
             "outputs": [{"type": "var", "name": "db"}],
         }
@@ -21,9 +22,9 @@ class Plugin(PluginBase):
     def __call__(self, **kwargs):
         run(
             inputs=self.plugin_config["inputs"],
-            outputs=self.plugin_config["outputs"],
+            outputs=self.plugin_config.get("outputs", None),
             svg_input_dir=self.plugin_config["svg_input_dir"],
-            svg_output_dir=self.plugin_config["svg_output_dir"],
+            svg_output_dir=self.plugin_config.get("svg_output_dir", None),
             base_url=self.plugin_config["base_url"],
             kwargs=kwargs,
         )
@@ -34,7 +35,9 @@ class Plugin(PluginBase):
         return "KanjiSVG"
 
 
-def run(inputs, outputs=None, *, svg_input_dir, svg_output_dir, base_url, kwargs=None):
+def run(
+    inputs, outputs=None, *, svg_input_dir, base_url, svg_output_dir=None, kwargs=None
+):
     """Add SVG representation of kanjis to database.
 
     Given a directory containing:
@@ -135,10 +138,15 @@ def run(inputs, outputs=None, *, svg_input_dir, svg_output_dir, base_url, kwargs
     outputs = outputs if outputs is not None else []
     kwargs = kwargs if kwargs is not None else {}
 
-    os.makedirs(svg_output_dir, exist_ok=True)
+    if svg_output_dir is not None:
+        os.makedirs(svg_output_dir, exist_ok=True)
 
     # Read db
     db = jsonstream.run(inputs=inputs, kwargs=kwargs)
+
+    has_export = svg_output_dir is not None and os.path.abspath(
+        svg_input_dir
+    ) != os.path.abspath(svg_output_dir)
 
     # Add SVG data
     for kanji, data in db.items():
@@ -150,7 +158,8 @@ def run(inputs, outputs=None, *, svg_input_dir, svg_output_dir, base_url, kwargs
         if os.path.isfile(filename):
             basename = os.path.basename(filename)
 
-            shutil.copy(filename, os.path.join(svg_output_dir, basename))
+            if has_export:
+                shutil.copy(filename, os.path.join(svg_output_dir, basename))
 
             media["svg"] = base_url + basename
 
